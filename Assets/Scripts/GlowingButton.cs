6ecbb3ded9b8404133391fbd1ec3f035fa35bc75
Chien Truong <tqc1234@gmail.com>
2022-12-22 00:27:22 +0700

    Update GlowingButton.cs

--- Assets/Scripts/GlowingButton.cs
+++ Assets/Scripts/GlowingButton.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class GlowingButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Boop");
